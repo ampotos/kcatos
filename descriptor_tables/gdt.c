b4df27fe5023a5412d5a98a56575852b3b015dc5
Kevin Soules <kevin@libdev.so>
2014-12-03 19:40:36 +0100

    gdt sort of works. Now the asm routine gdt_flush must be coded.

---
+++ descriptor_tables/gdt.c
@@ -0,0 +1,52 @@
+/*
+** gdt.c for  in /home/eax/dev/kcatos
+** 
+** Made by eax
+** Login   <soules_k@epitech.net>
+** 
+** Started on  Tue Dec  2 22:55:52 2014 eax
+** Last update Wed Dec  3 19:37:02 2014 eax
+*/
+
+#include "gdt.h"
+
+t_gdt_entry	gdt_entries[5];
+t_gdt_ptr	gdt_ptr;
+
+static void set_gdt_entrie(uint8_t idx,
+			   uint32_t base,
+			   uint32_t limit,
+			   uint16_t flags
+			   )
+{
+  /* assert */
+  gdt_entries[idx].limit_low = limit & 0xFFFF;
+  gdt_entries[idx].base_low = base & 0xFFFF;
+  gdt_entries[idx].base_middle = (base >> 16) & 0xFFFF;
+  gdt_entries[idx].type |= ((flags & CODE) != 0) << 3;
+  gdt_entries[idx].type |= ((flags & EXPAND_DOWN) != 0) << 2;
+  gdt_entries[idx].type |= ((flags & READ) != 0) << 1;
+  gdt_entries[idx].s = (flags & CODE_DATA) != 0;
+  gdt_entries[idx].dpl = flags & 0x3;
+  gdt_entries[idx].p = 1;
+  gdt_entries[idx].limit_high = (limit >> 16) & 0xFFFF;
+  gdt_entries[idx].avl = 0;
+  gdt_entries[idx].l = (flags & MODE_64B) != 0;
+  gdt_entries[idx].db = (flags & OP_SIZE_32B) != 0;
+  gdt_entries[idx].g = (flags & GRANUL_1K) != 0;
+  gdt_entries[idx].base_high = (base >> 24) & 0xFFFF;
+}
+
+static void init_gdt()
+{
+  gdt_ptr.base = (uint32_t)&gdt_entries;
+  gdt_ptr.limit = (sizeof (*gdt_entries) * 5) - 1;
+
+  set_gdt_entrie(0, 0, 0, 0);
+  set_gdt_entrie(1, 0, 0xFFFFFFFF, RING(0) | CODE_DATA | CODE | READ | OP_SIZE_32B | GRANUL_1K);
+  set_gdt_entrie(2, 0, 0xFFFFFFFF, RING(0) | CODE_DATA | DATA | WRITE | OP_SIZE_32B | GRANUL_1K);
+  set_gdt_entrie(3, 0, 0xFFFFFFFF, RING(3) | CODE_DATA | CODE | READ | OP_SIZE_32B | GRANUL_1K);
+  set_gdt_entrie(4, 0, 0xFFFFFFFF, RING(3) | CODE_DATA | DATA | WRITE | OP_SIZE_32B | GRANUL_1K);
+
+  gdt_flush(&gdt_ptr);
+}
---
+++ descriptor_tables/gdt.h
@@ -0,0 +1,58 @@
+/*
+** gdt.h for  in /home/eax/dev/kcatos
+** 
+** Made by eax
+** Login   <soules_k@epitech.net>
+** 
+** Started on  Tue Dec  2 21:13:49 2014 eax
+** Last update Wed Dec  3 19:34:06 2014 eax
+*/
+
+#ifndef GDT_H_
+#define GDT_H_
+
+#include <stdint.h>
+#include <assert.h>
+
+#define RING(n) (n & 0x03)
+#define CODE_DATA (4)
+#define GRANUL_1K (8)
+#define MODE_64B (16)
+#define CODE (32)
+#define DATA (0)
+#define READ (64)
+#define WRITE (64) /* This is not a bug */
+#define EXPAND_DOWN (128)
+#define CONFORMING (128) /* This is not a bug */
+#define OP_SIZE_32B (256)
+
+/*
+** Global descriptor entry
+** http://download.intel.com/design/processor/manuals/253668.pdf # 3.4.5 Segment Descriptors
+*/
+
+typedef struct s_gdt_entry
+{
+  uint16_t      limit_low;      /* limit[0:16] */
+  uint16_t      base_low;       /* base[0:16] */
+  uint8_t       base_middle;    /* base[16:24] */
+  uint8_t       type:4;         /* Segment type */
+  uint8_t       s:1;            /* Descriptor type (0 = system ; 1 = code or data) */
+  uint8_t       dpl:2;          /* Descriptor privilege level (ring 0-3) */
+  uint8_t       p:1;            /* Segment present */
+  uint8_t       limit_high:4;   /* limit[16:20] */
+  uint8_t       avl:1;          /* Available for use by system software */
+  uint8_t       l:1;            /* 64b mode segment (ia-32 mode only) */
+  uint8_t       db:1;           /* Default operation size (0 = 16b segment ; 1 = 32b segment) */
+  uint8_t       g:1;            /* Granularity (0 = 1b ; 1 = 1024b) */
+  uint8_t       base_high;      /* base[24:31] */
+}  __attribute__((packed)) t_gdt_entry;
+
+
+typedef struct s_gdt_ptr
+{
+  uint16_t	limit;
+  uint32_t	base;
+} __attribute__((packed)) t_gdt_ptr;
+
+#endif
