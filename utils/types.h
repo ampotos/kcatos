242e84b231465a6c2d38c5c0985647d1928994ab
Kevin Soules <kevin@libdev.so>
2014-12-27 11:18:30 +0100

    add a file providing u32/s32/u16/s16/u8/s8 plus size_t and NULL

---
+++ utils/types.h
@@ -0,0 +1,26 @@
+/*
+** types.h for  in /home/eax/dev/kcatos/memory
+** 
+** Made by eax
+** Login   <soules_k@epitech.net>
+** 
+** Started on  Sat Dec 27 11:13:44 2014 eax
+** Last update Sat Dec 27 11:16:54 2014 eax
+*/
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#define NULL (((void)*)0)
+
+typedef uint32_t	u32;
+typedef int32_t		s32;
+typedef uint16_t	u16;
+typedef int16_t		s16;
+typedef uint8_t		u8;
+typedef int8_t		s8;
+
+typedef u32		size_t;
+
+#endif
+
